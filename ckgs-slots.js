b4d6589ddd926ce57932dfb0c4193ea7fa659f91
parsavandy <62550885+parsavandy@users.noreply.github.com>
2022-11-16 07:12:21 +0330

    Update ckgs-slots.js

--- ckgs-slots.js
+++ ckgs-slots.js
@@ -2,8 +2,7 @@
 // @name           CKGS Slots Check by Parsa Vandy
 // @namespace      https://github.com/parsavandy/ckgsir
 // @author         Parsa Vandy
-
-// @date           11.15.2022
+// @description    :D
 // @version        1.0.0
 // @match 	https://www.ckgsir.com/*
 // @exclude 	*.css
@@ -13,16 +12,9 @@
 //***********************************************************************************************************
 
 
-document.querySelector('div').removeAttribute('loading');
-document.querySelector('div').setAttribute("loading", "");
 /* getElementByClass
 /**********************/
-var main, newElement,t;
-main = document.getElementById('body');
-var html = '<b><a href="https://www.ckgsir.com/getSlot?appointmentDate=21+November+2022&RadioType=regular" target="_blank">Start Tab</a></b>';
-document.querySelector(".percenage-dis").innerHTML = html;
-//first reload safhe
-t=setTimeout("attack()",300000);
+
 var player = document.createElement('audio');
     player.src = 'https://nickvisa.com/parsa/ckgs/alarm.mp3';
     player.preload = 'auto';
@@ -31,9 +23,17 @@ var player = document.createElement('audio');
 const text = 'slot available for this day';
 
 if (document.body.textContent.includes(text)) {
+   player.play();
+   clearTimeout(t);
 } else {
-        player.play();
-    clearTimeout(t);
 };
-//reload safhe
+
+var main, newElement,t;
+main = document.getElementById('body');
+
+var html = '<b><a href="https://www.ckgsir.com/getSlot?appointmentDate=21+November+2022&RadioType=regular" target="_blank">Start Tab</a></b>';
+document.querySelector(".percenage-dis").innerHTML = html;
+
+document.querySelector('div').removeAttribute('loading');
+
 setTimeout("location.reload(true);",300000);
